facda0509fe98e9200247a3182c7fb4813ce9d01
Jean-Eugenio <jeaneugeniodev2024@gmail.com>
2024-10-27 17:48:39 -0300

    Add files via upload

---
+++ src/components/Cabecalho/index.tsx
@@ -0,0 +1,11 @@
+import { Header } from './styles'
+
+const Cabecalho = () => {
+  return (
+    <Header>
+      <h1>EBAC Jobs</h1>
+    </Header>
+  )
+}
+
+export default Cabecalho
---
+++ src/components/Vaga/index.tsx
@@ -0,0 +1,29 @@
+import { Card, CardTitle, CardLink } from './styles'
+
+type Props = {
+  titulo: string
+  localizacao: string
+  nivel: string
+  modalidade: string
+  salarioMin: number
+  salarioMax: number
+  requisitos: string[]
+}
+
+const Vaga = (props: Props) => (
+  <Card>
+    <CardTitle>{props.titulo}</CardTitle>
+    <ul>
+      <li>Localizacao: {props.localizacao}</li>
+      <li>Senioridade: {props.nivel}</li>
+      <li>Tipo de contratacao: {props.modalidade}</li>
+      <li>
+        Salário: {props.salarioMin} - {props.salarioMax}
+      </li>
+      <li>Requisitos: {props.requisitos.join(', ')}</li>
+    </ul>
+    <CardLink href="#">Ver detalhes e candidatar-se</CardLink>
+  </Card>
+)
+
+export default Vaga
